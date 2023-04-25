d808de4ba03c5d1992cc731db7db8ccd84b16bfc
RizvanKarimov <karimovrizvan84@gmail.com>
2023-04-25 12:40:29 +0200

    filter function ready

--- src/pages/TypeResults.js
+++ src/pages/TypeResults.js
@@ -1,30 +1,32 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-
 const TypeResults = () => {
-    const [pokemons, setPokemons] = useState([]);
-    const location = useLocation();
-    console.log(location)
+  const [pokemons, setPokemons] = useState([]);
+  const location = useLocation();
+  console.log(location);
 
-    useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/type/${location.state}`)
-            .then((response) => response.json())
-            .then((json) => console.log(json));
-            // .then((json) => setPokemons(json));
-    }, []);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/type/${location.state}`)
+      .then((response) => response.json())
+      .then((json) => setPokemons(json.pokemon));
+  }, []);
 
-    return (
-        <section>
-            <p>{location.state}</p>
-            {pokemons.map((pokemon)=>{
-                return(
-                    <p>{pokemon.pokemon.pokemon}</p>
-                )
-            }
-            )}
-        </section>
-    );
-}
+  return (
+    <section>
+      <p>{location.state}</p>
+      {pokemons.map((pokemon, index) => {
+        const pokemonId = pokemon.pokemon.url.split("/")[6];
+        const myImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
+        return (
+          <>
+            <p key={index}>{pokemon.pokemon.name}</p>
+            <img src={myImage} alt={pokemon.name} />
+          </>
+        );
+      })}
+    </section>
+  );
+};
 
 export default TypeResults;
